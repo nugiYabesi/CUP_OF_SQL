40184be2477a91301ec31fc9905805c956a4255c
IYUKURI NUGI YABESI <nugiybes@gmail.com>
2025-04-12 09:54:23 -0700

    INSERTION OF DATA INTO THE TABLE

---
+++ SEPARATE FUNCTION SYNTAX/INSERTION OF VALUES IN COFFEE_SHOP_SALES.sql
@@ -0,0 +1,20 @@
+---- INSERTION OF VALUES IN COFFEE_SHOP_SALES---
+
+
+
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('IYUKURI YABES', 'Latte', 3.50, TO_TIMESTAMP('2025-04-01 08:00:00', 'YYYY-MM-DD HH24:MI:SS'),1);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('IYUKURI YABES', 'Espresso', 2.00, TO_TIMESTAMP('2025-04-01 08:15:00', 'YYYY-MM-DD HH24:MI:SS'),2);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('JAMES MANZI', 'Cappuccino', 3.00, TO_TIMESTAMP('2025-04-01 08:20:00', 'YYYY-MM-DD HH24:MI:SS'),3);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('ERIC MANIRAGUHA', 'Mocha', 4.00, TO_TIMESTAMP('2025-04-01 08:45:00', 'YYYY-MM-DD HH24:MI:SS'),4);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('KWIZERA INNOCENT', 'Latte', 3.50, TO_TIMESTAMP('2025-04-01 08:50:00', 'YYYY-MM-DD HH24:MI:SS'),4);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('MUGISHA BOB', 'Americano', 2.50, TO_TIMESTAMP('2025-04-01 09:00:00', 'YYYY-MM-DD HH24:MI:SS'),5);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity)  
+VALUES ('AMATA STELLA', 'Cappuccino', 3.00, TO_TIMESTAMP('2025-04-01 09:05:00', 'YYYY-MM-DD HH24:MI:SS'),8);
+
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('IYUKURI YABES', 'Tea', 3.50, TO_TIMESTAMP('2025-04-01 08:00:00', 'YYYY-MM-DD HH24:MI:SS'),11);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('JAMES MANZI', 'IKIVUGUTO', 3.00, TO_TIMESTAMP('2025-04-01 08:20:00', 'YYYY-MM-DD HH24:MI:SS'),13);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('ERIC MANIRAGUHA', 'INSHYUNSHYU', 4.00, TO_TIMESTAMP('2025-04-01 08:45:00', 'YYYY-MM-DD HH24:MI:SS'),14);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('KWIZERA INNOCENT', 'IGIKOMA', 3.50, TO_TIMESTAMP('2025-04-01 08:50:00', 'YYYY-MM-DD HH24:MI:SS'),14);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity) VALUES ('MUGISHA BOB', 'COFFEE', 2.50, TO_TIMESTAMP('2025-04-01 09:00:00', 'YYYY-MM-DD HH24:MI:SS'),15);
+INSERT INTO coffee_shop_sales (customer_name, drink_type, price, sale_time,quantity)  
+VALUES ('AMATA STELLA', 'LATTEE', 3.00, TO_TIMESTAMP('2025-04-01 09:05:00', 'YYYY-MM-DD HH24:MI:SS'),18);
